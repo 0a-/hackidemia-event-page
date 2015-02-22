213175bddea675f7da4257f46da355893f0f77f1
0a- <a@0a.io>
2015-02-22 19:41:53 +0800

    implemented social links for team members

--- public/js/home.js
+++ public/js/home.js
@@ -4,4 +4,8 @@ $(window).load(function(){
        var w = $(this).width();
        $(this).css("margin-left",(-(w-300)/2)+"px");
     });
+    $(".social").each(function(){
+        var eles = $(this).find(".social_block");
+        eles.css("width",1/eles.length*100+"%");
+    }); 
 });
